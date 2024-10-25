2fd0514b1c0a06783ac1942dc53b4fe7fadd0159
RubenGarciaViciana <rubgavi2001@gmail.com>
2024-10-25 14:06:55 +0200

    Funciones User

--- src/main/uab/tqs/bingo/model/User.java
+++ src/main/uab/tqs/bingo/model/User.java
@@ -5,19 +5,43 @@ public class User {
     private Cartoon cartoon;
 
     public User() {
-
+        this.name = "";
+        this.cartoon = null;
     }
 
     public User(String name, Cartoon cartoon) {
 
+        // Pre-condición
+        assert name != "" : "El no,bre no puede estar vacio";
+        assert cartoon != null : "El cartoon no puede ser nulo";
+
+        this.name = name;
+        this.cartoon = cartoon;
+
+        // Post-condición
+        assert this.name != "" : "No se ha cambiado el nombre";
+        assert this.cartoon != null : "No se ha cambiado el cartoon del objeto";
     }
 
     public void setName(String name) {
+        // Pre-condición
+        assert name != "" : "El no,bre no puede estar vacio";
 
+        this.name = name;
+
+        // Post-condición
+        assert this.name != "" : "No se ha cambiado el nombre";
     }
 
     public void setCartoon(Cartoon cartoon) {
 
+        // Pre-condición
+        assert cartoon != null : "El cartoon no puede ser nulo";
+
+        this.cartoon = cartoon;
+
+        // Post-condición
+        assert this.cartoon != null : "No se ha cambiado el cartoon del objeto";
     }
 
     public String getName() {
